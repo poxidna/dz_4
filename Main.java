9cc192681393c4c1186700ea276192c724b47103
poxidna <poxidna@ukr.net>
2023-04-02 17:09:59 +0300

    dz_added_files

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Second var = new Second();
+
+        var.printA();
+
+    }
+}
---
+++ Second.java
@@ -0,0 +1,11 @@
+public class Second {
+
+    int a = 5;
+
+    public int printA (){
+        System.out.println(String.format("------<значення %s>--------", a));
+        return a;
+
+    }
+
+}
